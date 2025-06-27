afb7dfc25834c7407ea09d959d0dae166872ca9e
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-26 23:28:38 -0500

    init

---
+++ src/main/java/com/isai/demo_relaciones_jpa/DemoRelacionesJpaApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_relaciones_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRelacionesJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRelacionesJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_relaciones_jpa/DemoRelacionesJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_relaciones_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRelacionesJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
