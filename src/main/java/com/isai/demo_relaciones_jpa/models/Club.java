20a9213b3c0c77b0f1e512eb5d0177ce0258781b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-27 00:09:50 -0500

    relacion de uno a uno

--- src/main/java/com/isai/demo_relaciones_jpa/models/Club.java
+++ src/main/java/com/isai/demo_relaciones_jpa/models/Club.java
@@ -1,9 +1,12 @@
 package com.isai.demo_relaciones_jpa.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,10 +18,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Club {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idClub;
-    
+
     private String nombre;
+
+    @OneToOne(targetEntity = Entrenador.class, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "id_entrenador")
+    private Entrenador entrenador;
+
 }
--- src/main/java/com/isai/demo_relaciones_jpa/models/Entrenador.java
+++ src/main/java/com/isai/demo_relaciones_jpa/models/Entrenador.java
@@ -1,6 +1,9 @@
 package com.isai.demo_relaciones_jpa.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
@@ -12,6 +15,8 @@ import lombok.*;
 @Table(name = "entrenador")
 public class Entrenador {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idEntrenador;
     
     private String nombre;
